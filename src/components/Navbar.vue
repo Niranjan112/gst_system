3a1d16b13f55a01812a3bd8d4ab20cac5262c86b
niranjan112 <niranjan112>
2019-10-03 07:46:48 +0530

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-toolbar extended extension-height="2" color="indigo darken-4 white--text">
+      
+        <img src="../assets/logo.png" alt="avatar"/>
+      
+      <v-toolbar-title class="display-1">Goods & Service Tax</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-item>
+        <v-btn text large class="white--text">Home</v-btn>
+        <v-btn text large class="white--text">Service</v-btn>
+        <v-btn text large class="white--text">GST Law</v-btn>
+        <v-btn text large class="white--text">Downloads</v-btn>
+        <v-btn text large class="white--text">Search Taxpayer</v-btn>
+        <v-btn text large class="white--text">Help</v-btn>
+        <v-btn text large class="white--text">Contact Us</v-btn>
+      </v-toolbar-item>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div>
+            <v-row align="center" justify="center">
+                <v-img
+                    src="../assets/gst_main.jpg"
+                    alt="gst_main"
+                    aspect-ratio="1"
+                    max-width="99%"
+                    max-height="400"
+                ></v-img>
+            </v-row>
+        </div>
+
+        <div>
+            <v-container grid-list-md text-xs-center>
+                <v-layout row wrap>
+                    <v-flex>
+                        <v-card flat max-width="344" class="mx-auto" outlined>
+                            <v-card-title class="indigo darken-4 white--text justify-center">
+                                News | Updates | Events
+                            </v-card-title>
+                            <v-divider></v-divider>
+                            <v-card-text class="indigo lighten-4">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius vitae quod mollitia corporis exercitationem doloribus, laboriosam inventore similique, nostrum, fugit soluta perferendis sunt aperiam! Illo consequatur beatae magni iure dolorum?
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+
+                    <v-flex>
+                        <v-card flat width="700" class="mx-auto" outlined>
+                            <v-card-title class="indigo darken-4 white--text text-center">
+                                Popular Helpful Topics
+                            </v-card-title>
+                            <v-divider></v-divider>
+                            <v-card-text class="indigo lighten-4">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto officiis quod consequuntur enim cum non pariatur molestiae aperiam aliquam distinctio incidunt, magnam vero sed. Commodi omnis mollitia impedit fuga quas.
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+
+};
 </script>
